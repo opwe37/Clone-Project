03a0ca95903590ec3cc161654c7375517183ff61
opwe37 <opwe37@gmail.com>
2021-08-29 16:07:55 +0900

    [zoom] websocket to socket.io

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
+// import WebSocket from 'ws';
 
 const app = express();
 
@@ -9,40 +10,48 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+// app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = SocketIO(server);
 
-// DB 역할을 수행할 녀석
-const sockets = [];
+io.on('connection', (socket) => {
+    console.log(socket); // socket 안에 sockets 라는 연결된 소켓'들'을 관리하는 곳이 있음!!
+})
 
-// wss.on(event, fn)
-// - event: 정해진 이벤트가 있음
-// - fn: 이벤트 발생 시, 수행할 함수
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";    // socket 이 객체라는 것을 이용해서 새로운 속성을 내가 부여할 수 있음
-    console.log("Connected to Browser ✔");
-    socket.on("close", () => { console.log("Disconnected from the Browser ❌")});
+
+// websocket 을 사용할 때 썼던 코드
+// const wss = new WebSocket.Server({ server });
+
+// // DB 역할을 수행할 녀석
+// const sockets = [];
+
+// // wss.on(event, fn)
+// // - event: 정해진 이벤트가 있음
+// // - fn: 이벤트 발생 시, 수행할 함수
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";    // socket 이 객체라는 것을 이용해서 새로운 속성을 내가 부여할 수 있음
+//     console.log("Connected to Browser ✔");
+//     socket.on("close", () => { console.log("Disconnected from the Browser ❌")});
     
-    socket.on("message", (message) => {
+//     socket.on("message", (message) => {
 
-        const {type, payload} = JSON.parse(message.toString());
+//         const {type, payload} = JSON.parse(message.toString());
         
-        switch (type) {
-            case 'message':
-                const targetSockets = sockets.filter(aSocket => socket != aSocket);
-                targetSockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${payload}`));
-                break;
-            case 'nickname':
-                socket["nickname"] = payload;
-                break;
-        }
-    });
-});
+//         switch (type) {
+//             case 'message':
+//                 const targetSockets = sockets.filter(aSocket => socket != aSocket);
+//                 targetSockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${payload}`));
+//                 break;
+//             case 'nickname':
+//                 socket["nickname"] = payload;
+//                 break;
+//         }
+//     });
+// });
 
 server.listen(3000, handleListen);
