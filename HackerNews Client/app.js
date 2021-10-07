d3adaf082e4b71f5b33dc2044722bb2c1226531a
opwe37 <opwe37@gmail.com>
2021-10-07 11:41:10 +0900

    ch03_04

--- HackerNews Client/app.js
+++ HackerNews Client/app.js
@@ -1,46 +1,54 @@
-const container = document.getElementById('root');
+// 기존 JS에서 HTML을 조작하는 방식: DOM API 적극 활용
+// 기존 방식의 문제점: HTML의 마크업 구조가 명확히 들어나지 않음
+//                   (코드가 실제로 어떻게 그려질지에 대한 혼란 야기)
+// 개선 방안: DOM API 사용 부분을 최소화하고 가능한 한 문자열로 대체
 
-const ajax = new XMLHttpRequest();
+const container = document.getElementById('root');
 const content = document.createElement('div');
 
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url) {
+    const ajax = new XMLHttpRequest();
 
-const newsFeed = JSON.parse(ajax.response);
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 
 const ul = document.createElement('ul');
 
-// 이벤트: 코드 상으로는 실행 시점을 알 수 없는 상황에서 사용하는 브라우저의 기능
 window.addEventListener('hashchange', () => {
-    // location: 주소 정보를 갖고 있는 객체
-    // substr(idx): 스트링의 부분만 가져와서 쓰고 싶을때
     const id = location.hash.substr(1);
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
 
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     
     const title = document.createElement('h1');
     title.innerHTML = newsContent.title;
 
-    // title이 누적되서 보여지는 버그를 잡기위해 추가
     content.innerHTML = '';
 
     content.appendChild(title);
 });
 
 for (let i = 0; i < 10; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    // 문자열을 실제 HTML요소로 변환시켜 저장하고 있을 임시 태그
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title}(${newsFeed[i].comments_count})
+            </a>
+        </li>
+    `;
 
-    a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`;
-    a.href = `#${newsFeed[i].id}`;
-
-    li.appendChild(a);
-    ul.appendChild(li);
+    // 동일한 코드 
+    // ul.appendChild(div.children[0]);
+    ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
