1c9c5b02a1333b46914a1a9006fdaaf4a0b1c8bd
opwe37 <opwe37@gmail.com>
2021-10-10 17:41:18 +0900

    ch03_05

--- HackerNews Client/app.js
+++ HackerNews Client/app.js
@@ -1,7 +1,5 @@
-// 기존 JS에서 HTML을 조작하는 방식: DOM API 적극 활용
-// 기존 방식의 문제점: HTML의 마크업 구조가 명확히 들어나지 않음
-//                   (코드가 실제로 어떻게 그려질지에 대한 혼란 야기)
-// 개선 방안: DOM API 사용 부분을 최소화하고 가능한 한 문자열로 대체
+// 기존의 문제점: 페이지 구분이 안됨
+// 개선 방안: 라우터 기능 추가
 
 const container = document.getElementById('root');
 const content = document.createElement('div');
@@ -18,38 +16,53 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
+// newsFeed(): 글 목록을 보여주는 화면을 그리는 함수
+// 필요할 때, 이 함수를 불러 글 목록 화면을 그릴 수 있도록 함수화
+// 재사용성↑
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-const ul = document.createElement('ul');
+    newsList.push('<ul>');
+    for (let i = 0; i < 10; i++) {
+        newsList.push(`
+            <li>
+                <a href="#${newsFeed[i].id}">
+                    ${newsFeed[i].title}(${newsFeed[i].comments_count})
+                </a>
+            </li>
+        `);
+    }
+    newsList.push('</ul>');
 
-window.addEventListener('hashchange', () => {
-    const id = location.hash.substr(1);
-
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    
-    const title = document.createElement('h1');
-    title.innerHTML = newsContent.title;
-
-    content.innerHTML = '';
-
-    content.appendChild(title);
-});
-
-for (let i = 0; i < 10; i++) {
-    // 문자열을 실제 HTML요소로 변환시켜 저장하고 있을 임시 태그
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <li>
-            <a href="#${newsFeed[i].id}">
-                ${newsFeed[i].title}(${newsFeed[i].comments_count})
-            </a>
-        </li>
-    `;
-
-    // 동일한 코드 
-    // ul.appendChild(div.children[0]);
-    ul.appendChild(div.firstElementChild);
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const id = location.hash.substr(1);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+    
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash;
+
+    // 왜 동작하지? newsDetail()에서 id 값 가져올때는 '#'문자 때문에 1번 인덱스부터 읽었는데?
+    // location.hash는 실제 주소의 해시에 '#'만 있을 경우에는 빈 값을 반환해주는 특징이 있음!!
+    if (routePath === '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
