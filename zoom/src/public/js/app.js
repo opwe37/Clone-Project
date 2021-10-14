046e89da8a1dbbbcff27a33976ba3ab9a979716c
opwe37 <opwe37@naver.com>
2021-10-15 00:35:17 +0900

    #3.1 Call Controls

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -3,6 +3,7 @@ const socket = io();
 const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
+const camerasSelect = document.getElementById('cameras');
 
 const store = {
     muted: false,
@@ -18,14 +19,33 @@ async function getMedia() {
             video: true,
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.log(e)
+    }
+}
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind == 'videoinput');
+        
+        let template = `<option value="__id__">__label__</option>`;
+        camerasSelect.innerHTML = '';
+        for (let camera of cameras) {
+            camerasSelect.innerHTML += template.replace('__id__', camera.deviceId).replace('__label__', camera.label);
+        }
+    } catch (err) {
+        console.log(err);
     }
 }
 
 getMedia();
 
 function handleMuteClick() {
+    myStream
+        .getAudioTracks()
+        .forEach(track => track.enabled = !track.enabled)
     if (!store.muted) {
         muteBtn.innerText = 'Unmute';
         store.muted = true;
@@ -36,6 +56,9 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream
+        .getVideoTracks()
+        .forEach(track => track.enabled = !track.enabled)
     if (store.cameraOff) {
         cameraBtn.innerText = 'Turn Camera Off';
         store.cameraOff = false;
