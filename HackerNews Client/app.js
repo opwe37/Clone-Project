58bfa45303fe7ea8b178e2dcac1a5ae688689449
opwe37 <opwe37@gmail.com>
2021-10-14 11:22:39 +0900

    ch03_06

--- HackerNews Client/app.js
+++ HackerNews Client/app.js
@@ -1,11 +1,17 @@
-// 기존의 문제점: 페이지 구분이 안됨
-// 개선 방안: 라우터 기능 추가
+// 페이징 구현
 
 const container = document.getElementById('root');
 const content = document.createElement('div');
 
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+// 상태값 관리를 위한 저장소 선언
+// 특정 페이징 위치에서 뉴스를 클릭해서 본 이후, 뒤로가기 했을때
+// 어떤 페이징 위치를 보여줄지 기억하기위한 상태값 관리가 필요
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     const ajax = new XMLHttpRequest();
@@ -16,18 +22,15 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-// newsFeed(): 글 목록을 보여주는 화면을 그리는 함수
-// 필요할 때, 이 함수를 불러 글 목록 화면을 그릴 수 있도록 함수화
-// 재사용성↑
 function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newsList = [];
 
     newsList.push('<ul>');
-    for (let i = 0; i < 10; i++) {
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                     ${newsFeed[i].title}(${newsFeed[i].comments_count})
                 </a>
             </li>
@@ -35,18 +38,28 @@ function newsFeed() {
     }
     newsList.push('</ul>');
 
+    // 방어 코드 작성 (newFeed에서 제공하는 범위 밖으로 이동을 막기 위함)
+    const min_page = 1;
+    const max_page = newsFeed.length / 10;
+
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > min_page ? store.currentPage-1 : min_page}">이전 페이지</a>
+            <a href="#/page/${store.currentPage < max_page ? store.currentPage+1 : max_page}">다음 페이지</a>
+        </div>
+    `);
     container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -54,9 +67,10 @@ function newsDetail() {
 function router() {
     const routePath = location.hash;
 
-    // 왜 동작하지? newsDetail()에서 id 값 가져올때는 '#'문자 때문에 1번 인덱스부터 읽었는데?
-    // location.hash는 실제 주소의 해시에 '#'만 있을 경우에는 빈 값을 반환해주는 특징이 있음!!
     if (routePath === '') {
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) {
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
